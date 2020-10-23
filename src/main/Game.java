3364e61ba15a52025b985384b46d5149c7f65c28
Klaiton <klaitonricken@hotmail.com>
2020-10-23 07:01:30 -0300

    Game engine, entities e spritesheets

---
+++ src/main/Game.java
@@ -0,0 +1,139 @@
+package main;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import entities.Entity;
+import entities.Player;
+import graficos.Spritsheet;
+
+public class Game extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 1L;
+	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160; 
+	private final int SCALE  = 3;
+	
+	private BufferedImage image;
+	
+	public java.util.List<Entity> entities;
+	public Spritsheet spritesheet;
+	
+	public Game() {
+		
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		initFrame();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		
+		entities =  new ArrayList<Entity>();
+		spritesheet =  new Spritsheet("/spritesheet.png");
+		
+		Player player  = new Player(0, 0, 16, 16, spritesheet.getSprite(32, 0, 16, 16));
+		
+		entities.add(player);
+	}
+	
+	public static void main(String args[]) {
+		Game game = new Game();
+		game.start();
+	}
+	
+	public void initFrame() {
+		frame = new JFrame("Game #1");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning =  true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void tick() {
+		
+		for(int i = 0; i<entities.size(); i++) {
+			Entity e = entities.get(i);
+			
+			if (e instanceof Player) {
+				
+			}
+			
+			e.tick();
+		}
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(0,0,0));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		
+		for(int i = 0; i<entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
+		
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
+	}
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amoutOfTicks =60.0;
+		double ns = 1000000000/ amoutOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+			
+		}
+		stop();
+	}
+
+}
