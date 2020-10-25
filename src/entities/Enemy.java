d71b7c1a5f02d3c9b60de21696a5309cb4e01d1d
Klaiton <klaitonricken@hotmail.com>
2020-10-25 00:20:27 -0300

    Ajuste na animacao do inimigo

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -9,34 +9,33 @@ import world.World;
 
 public class Enemy extends Entity{
 	
-	private double speed = 0.7;
+	private double speed = Game.rand.nextDouble();
 	
 	private int maskx = 8, masky = 8, maskw = 8, maskh = 14;
 	
-	private BufferedImage [] rightPlayer;
-	private BufferedImage [] leftPlayer;
-	private BufferedImage [] upPlayer;
-	private BufferedImage [] downPlayer; 
+	private BufferedImage [] rightEnemy;
+	private BufferedImage [] leftEnemy;
+	private BufferedImage [] upEnemy;
+	private BufferedImage [] downEnemy; 
 	public int rightDir = 0, leftDir = 1, upDir = 2, downDir = 3;
 	public int dir = rightDir;
 	private boolean moved = false;
-	private int frames = 0, maxFrames = 5, index = 0, maxIndex = 3;
+	private int frames = 0, maxFrames = 20, index = 0, maxIndex = 3;
 
 	public Enemy(double x, double y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, null);
 		
-		rightPlayer = new BufferedImage[4];
-		leftPlayer = new BufferedImage[4];
-		upPlayer = new BufferedImage[4];
-		downPlayer = new BufferedImage[4];
+		rightEnemy = new BufferedImage[4];
+		leftEnemy = new BufferedImage[4];
+		upEnemy = new BufferedImage[4];
+		downEnemy = new BufferedImage[4];
 		
 		for(int i = 0; i<4; i++) {
-			rightPlayer[i] = Game.spritesheet.getSprite(96 + (i*16), 0, 16, 16);
-			leftPlayer[i] = Game.spritesheet.getSprite(96 + (i*16), 16, 16, 16);
-			upPlayer[i] = Game.spritesheet.getSprite(96 + (i*16), 32, 16, 16);
-			downPlayer[i] = Game.spritesheet.getSprite(96 + (i*16), 48, 16, 16);
+			rightEnemy[i] = Game.spritesheet.getSprite(96 + (i*16), 0, 16, 16);
+			leftEnemy[i] = Game.spritesheet.getSprite(96 + (i*16), 16, 16, 16);
+			upEnemy[i] = Game.spritesheet.getSprite(96 + (i*16), 32, 16, 16);
+			downEnemy[i] = Game.spritesheet.getSprite(96 + (i*16), 48, 16, 16);
 		}
-		
 	}
 	
 	public boolean isColidding(int xNext, int yNext) {
@@ -61,27 +60,30 @@ public class Enemy extends Entity{
 		
 		moved = false;
 		
-		if(Game.rand.nextInt(100) < 40) {
+		if(speed<0.3) 
+			speed +=0.25;
+		else if(speed>0.8)
+			speed -= 0.25;
+		
 			if((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY()) && !isColidding((int)(x+speed), this.getY())) {
+				moved =  true;
 				x += speed;
 				dir = rightDir;
-				moved =  true;
 			}else if ((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY()) && !isColidding((int)(x-speed), this.getY())){
+				moved =  true;
 				x -= speed;
 				dir = leftDir;
-				moved =  true;
 			}
 			
 			if((int)y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed)) && !isColidding(this.getX(), (int)(y+speed))) {
+				moved =  true;
 				y += speed;
 				dir = downDir;
-				moved =  true;
 			}else if ((int)y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed)) && !isColidding(this.getX(), (int)(y-speed))){
+				moved =  true;
 				y -= speed;
 				dir = upDir;
-				moved =  true;
 			}
-		}
 		
 		if(moved) {
 			frames++;
@@ -101,13 +103,13 @@ public class Enemy extends Entity{
 		super.render(g);
 		
 		if(dir == rightDir)
-			g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			g.drawImage(rightEnemy[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
 		else if (dir == leftDir)
-			g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			g.drawImage(leftEnemy[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
 		else if(dir == upDir)
-			g.drawImage(upPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			g.drawImage(upEnemy[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
 		else if(dir == downDir)
-			g.drawImage(downPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			g.drawImage(downEnemy[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
 	
 //		g.setColor(Color.black);
 //		g.fillRect(this.getX() - Camera.x, this.getY() - Camera.y, 16, 16);
