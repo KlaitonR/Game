4ede428fe2e9adf58d5bb9bfd3d32a11a7baaddb
Klaiton <klaitonricken@hotmail.com>
2020-10-25 20:28:52 -0300

    Finalização de UI

--- src/graficos/UI.java
+++ src/graficos/UI.java
@@ -1,8 +1,8 @@
 package graficos;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-
 import main.Game;
 
 public class UI {
@@ -10,11 +10,14 @@ public class UI {
 	public void render(Graphics g) {
 		
 		g.setColor(Color.black);
-		g.fillRect(7, 3, 52, 10);
+		g.fillRect(7, 3, 72, 10);
 		g.setColor(Color.red);
-		g.fillRect(8, 4, 50, 8);
+		g.fillRect(8, 4, 70, 8);
 		g.setColor(Color.green);
-		g.fillRect(8, 4, (int)((Game.player.life/Game.player.maxLife)*50), 8);
+		g.fillRect(8, 4, (int)((Game.player.life/Game.player.maxLife)*70), 8);
+		g.setColor(Color.white);
+		g.setFont(new Font("arial", Font.BOLD, 8));
+		g.drawString((int)Game.player.life + "/" + (int)Game.player.maxLife, 30, 11);
 		
 	}
 
